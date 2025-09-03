1ff9aff44ef2de5630b40efcc76d990283232e24
w0nkest <nmdvoret@gmail.com>
2025-09-03 22:55:25 +0300

    тут все по стилю сделано для людей

--- laba3/Form1.Designer.cs
+++ laba3/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonArtem = new System.Windows.Forms.Button();
+            this.buttonChel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonArtem
+            // 
+            this.buttonArtem.Location = new System.Drawing.Point(89, 50);
+            this.buttonArtem.Name = "buttonArtem";
+            this.buttonArtem.Size = new System.Drawing.Size(141, 49);
+            this.buttonArtem.TabIndex = 0;
+            this.buttonArtem.Text = "Артем";
+            this.buttonArtem.UseVisualStyleBackColor = true;
+            // 
+            // buttonChel
+            // 
+            this.buttonChel.Location = new System.Drawing.Point(89, 154);
+            this.buttonChel.Name = "buttonChel";
+            this.buttonChel.Size = new System.Drawing.Size(141, 49);
+            this.buttonChel.TabIndex = 1;
+            this.buttonChel.Text = "Чел";
+            this.buttonChel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(321, 276);
+            this.Controls.Add(this.buttonChel);
+            this.Controls.Add(this.buttonArtem);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonArtem;
+        private System.Windows.Forms.Button buttonChel;
     }
 }
 
