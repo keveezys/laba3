5062ee0ecfa48496541d64dc96f7e808af51768b
w0nkest <nmdvoret@gmail.com>
2025-09-03 22:59:48 +0300

    новая ветка

--- zadanie_palicha/Program.cs
+++ zadanie_palicha/Program.cs
@@ -17,6 +17,7 @@ namespace zadanie_palicha
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
